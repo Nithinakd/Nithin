525c76aa161e1d6f1f90fa5713f90fbf23b97134
Nithinakd <111360252+Nithinakd@users.noreply.github.com>
2022-08-24 17:34:19 +0530

    Add files via upload

---
+++ Aug23/P7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dividend");
+            int.TryParse(Console.ReadLine(), out int dividend);
+
+            Console.WriteLine("Enter the divisor");
+            int.TryParse(Console.ReadLine(), out int divisor);
+
+            Console.WriteLine(Divide(dividend, divisor));
+        }
+
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by 0");
+                throw; 
+            }
+        }
+    }
+}
