b9369f0dad71fc5264672ca2fbabdf367e7a35e2
Nithinakd <111360252+Nithinakd@users.noreply.github.com>
2022-09-04 16:48:35 +0530

    Add files via upload

---
+++ sept1/Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\Admin\Desktop\MainProject\students.txt";
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filepath).ToList();
+            Console.ReadLine();
+        }
+    }
+}
