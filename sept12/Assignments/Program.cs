f4016a19d2cca60010842764a2acc47609d3c626
Nithinakd <111360252+Nithinakd@users.noreply.github.com>
2022-09-13 14:09:54 +0530

    Add files via upload

---
+++ sept12/Assignments/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\sept12\\ClassLibrary1\\bin\\Debug\\ClassLibrary1.dll";
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            Type t = null;
+
+            foreach (var item in types)
+            {
+                t = asm.GetType(item.FullName);
+
+                object obj = Activator.CreateInstance(t);
+                item.InvokeMember("cube", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { 10.0 });
+                item.InvokeMember("sqrt", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { 10.0 });
+
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("-------------");
+        }
+    }
+}
